8c7300bbda9d03ba7b798f9755a3a081b6182420
Beyza Nur ALTINOK <51121202+QueenOfVenus@users.noreply.github.com>
2024-10-21 14:41:14 +0300

    Create 5_Day_ Challenge.py

---
+++ 5_Day_ Challenge.py
@@ -0,0 +1,72 @@
+////////////////////////////// If - Else Statement Of Python ///////////////////////////////////
+
+myName = input("What's your name?: ")
+if myName == "David":
+
+myName = input("What's your name?: ")
+if myName == "David":
+  print("Welcome Dude!")
+
+
+myName = input("What's your name?: ")
+if myName == "David":
+ print("Welcome Dude!")
+ print("You're just the baldest dude I've ever seen")
+else:
+ print("Who on earth are you?!")
+
+//////Fix The Code//////
+
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs = "cat":
+  print("Meow")
+else:
+  print("Woof")
+
+
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat"
+  print("Meow")
+else:
+  print("Woof")
+
+
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+  print("Meow")
+else:
+print("Woof")
+
+drink = input("Do you prefer coffee or tea?)
+if drink = "coffee"
+  print("Tea is better.")
+    else:
+  print("Excellent choice.")
+
+//////////// My Code ///////////////
+
+print("Marvel Movie Character Creator")
+print()
+print("--")
+print()
+print("")
+
+
+hanging = input("Do you like 'hanging around'?: ")
+gravelly = input("Do you have a 'gravelly' voice?: ")
+marvelous = input("Do you often feel 'Marvelous'?: ")
+
+if hanging == "no" :
+  print("Then you're not Spider-man")
+  
+elif gravelly == "no" :
+  print("Aww, then you're not Korg")
+
+elif marvelous == "yes" :
+  print("Aha! You're Captain Marvel! Hi!")
+
+else :
+  print("who the hell are you?")
+
+
+///////////////// Day 5 Finish /////////////////
