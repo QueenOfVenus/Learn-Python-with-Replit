0397e24350975626df8edda17aae9608c24f0d48
Beyza Nur ALTINOK <51121202+QueenOfVenus@users.noreply.github.com>
2024-10-21 14:15:53 +0300

    Create 2_Day_ Challenge.py

---
+++ 2_Day_ Challenge.py
@@ -0,0 +1,68 @@
+////////////////////////////// External Data Entry Of Python ///////////////////////////////////
+
+input("What's your name?: ")
+
+myName = input("What's you name? ")
+myAge = input("How old are you? ")
+print("Gee, that's REALLY OLD")
+replit = input("Do you like Replit? ")
+print("OF COURSE YOU DO!")
+
+print()
+print("So, you are")
+print(myName)
+print("and are the ripe old age of")
+print(myAge)
+print("and clearly think that Replit is")
+print(replit)
+
+//////Fix The Code//////
+
+my variable = input("WHO GOES THERE? ")
+print(my variable)
+
+myGrandma = input("How's your Grandma doing? 😘 ")
+print(mygrandma)
+
+myLunch = input("What are you having for lunch? ")
+print("Hmm, it sounds like you really should just order")
+print("myLunch")
+print("as soon as possible!")
+
+print("Definitely not a Phishing Scam")
+print()
+input("Your Name")
+print("Thanks for logging in")
+print(myName)
+cardNumber = input("What is your credit card number?")
+print("Thanks, I definitely wont put")
+print("cardNumber")
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maidenName input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print(maidenName)
+print("was your Mum's maiden name. Not because the bank requested it or anything, honest.")
+
+//////////// My Code ///////////////
+
+name = input("Hello! What's your name? ")
+food = input("So, what's your favourite food? ")
+music = input("Great! Now, can you say me your favourite music? ")
+live = input("Absolutely, where do you live? ")
+
+
+print("Okey okey! Listen me. ")
+
+print("You are")
+print(name)
+print("You're probably hungry for")
+print(food)
+print("and you're definitely getting your groove on to")
+print(music)
+print("living in the amazing")
+print(live)
+
+///////////////// Day 2 Finish /////////////////
