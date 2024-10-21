9e0e0ceecc9323cccb2fb3a3aff88b0933b09d01
Beyza Nur ALTINOK <51121202+QueenOfVenus@users.noreply.github.com>
2024-10-21 14:53:44 +0300

    Create 8_Day_ Challenge.py

---
+++ 8_Day_ Challenge.py
@@ -0,0 +1,39 @@
+////////////////////////////// Affirmations (or insults) Generator Of Python ///////////////////////////////////
+
+
+
+//////Fix The Code//////
+
+name = input("Name: ")
+if name == "Dave" or "dave":
+  print("Hi Dave")
+
+
+name = input("Name: ")
+if name == "Dave" or name == "dave":
+  print("Hi Dave")
+
+//////////// My Code ///////////////
+
+print("Hello. What's your name?")
+name = input("")
+if name == "Dolu" or name == "dolu" :
+  print ("Oh hi " , name , ".! How your feeling today honey? ")
+  feel = input("")
+  if feel == "not good" or feel == "I see better days" or feel == "I'm not feeling good" :
+    print("I understand u very well. You are obviously goin through difficult times. ")
+    print("In times like these, know that the people who love you, especially urself,")
+    print(" are by your side. ")
+    print("Remember the good moments u spent with thm. Think of the innocent children ")
+    print("in Palestine and remember to be grateful for everything you have now.")
+  elif feel == "I'm good" or feel == "perfect" or feel == "Awesome" :
+    print("Dude, this is an amazing thing, feeling the beauty and the chirping ")
+    print("of the birds. What anyone wouldn't give to feel this way. ")
+    print("You are great. I adore you. <3")
+  else :
+    print("I'm glad you're feeling something. Because the Palestinian people cannot ")
+    print("feel it anymore .·°՞(¯□¯)՞°·. Life is too short to feel bad")
+else :
+  print("I know are not you honey. Get out! Get Out here now!!!")
+
+///////////////// Day 8 Finish /////////////////
