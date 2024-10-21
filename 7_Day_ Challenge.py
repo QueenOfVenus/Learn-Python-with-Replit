c844c2809b92ad2acca983429ba1626b444d4f10
Beyza Nur ALTINOK <51121202+QueenOfVenus@users.noreply.github.com>
2024-10-21 14:47:38 +0300

    Create 7_Day_ Challenge.py

---
+++ 7_Day_ Challenge.py
@@ -0,0 +1,70 @@
+////////////////////////////// Nesting Of Python ///////////////////////////////////
+
+tvShow = input("What is your favorite tv show? ")
+if tvShow == "peppa pig":
+  print("Ugh, why?")
+  faveCharacter = input("Who is your favorite character? ")
+  if faveCharacter == "daddy pig":
+    print("Right answer")
+  else:
+    print("Nah, Daddy Pig's the greatest")
+elif tvShow == "paw patrol":
+  print("Aww, sad times")
+else:
+  print("Yeah, that's cool and all…")
+
+//////Fix The Code//////
+
+tvShow = input("What is your favourite tv show? ")
+if tvShow == "peppa pig":
+  print("Ugh, why?")
+  faveCharacter = input("Who is your favourite character? ")
+  if faveCharacter == "daddy pig":
+    print("Right answer")
+else:
+  print("Nah, Daddy Pig's the greatest")
+elif tvShow == "paw patrol":
+  print("Aww, sad times")
+else:
+  print("Yeah, that's cool and all…")
+
+
+order = input(What would you like to order: pizza or hamburger? ")
+if order = "hamburger":
+print("Thank you.")
+  cheese = input("Do you want cheese?")
+  if cheese == "yes":
+  print("You got it.")
+else: 
+    print("No cheese it is.")
+elif order == pizza:
+  print("Pizza coming up.")
+  toppings = input("Do you want pepperoni on that?")
+  if toppings = "yes"
+    print("We will add pepperoni.")
+else:
+  print"Your pizza will not have pepperoni.")
+
+//////////// My Code ///////////////
+
+tvShow = input("What is your favorite TV Show? :  ")
+
+if tvShow == "Kızılcık Şerbeti":
+  print("Is there no other series left?")
+  hateChar = input("So, Which character do you hate the most? :  ")
+  if hateChar == "Fatih":
+    print("Ugh! (งಠヮಠ)ง It's really disgusting, Pembe is so bad too")
+  else:
+    print("Oh he's such a sweet person")
+elif tvShow == "Bahar":
+  print("Dude, you understand good TV series. ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧ ")
+  favChar = input("Me too, Which character do you find most innocent?")
+  if favChar == "Parla":
+    print("You're right, poor Parla")
+  else:
+    print("I think everyone seems innocent except Timur, Rengin and Nevra.")
+else:
+  print("I don't know this")
+
+
+///////////////// Day 7 Finish /////////////////
