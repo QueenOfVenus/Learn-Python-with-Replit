f461706260f351cabab86e73aca4cac390962f5d
Beyza Nur ALTINOK <51121202+QueenOfVenus@users.noreply.github.com>
2024-10-21 14:21:23 +0300

    Create 3_Day_ Challenge.py

---
+++ 3_Day_ Challenge.py
@@ -0,0 +1,47 @@
+////////////////////////////// Print Formats Of Python ///////////////////////////////////
+
+myName = input("What's your name? ")
+myLunch = input("What are you having for lunch? ")
+print(myName, "is going to be chowing down on", myLunch, "very soon!")
+
+number = input("Give me a number: ")
+group = input("Give me a collective noun for a group of things: ")
+thing = input("Give me the name of a weird or wacky thing: ")
+print("No I don't think that", number, "is a", group, "of", thing,". That's just odd.")
+
+//////Fix The Code//////
+
+yourName = input("Name: ")
+whatYear = input("What year is it?: ")
+print(yourName "thinks it is" whatYear)
+
+yourName = input("Name: ")
+whatYear = input("What year is it?: ")
+print("yourName, thinks it is, whatYear")
+
+print("=== Your Song Generator ===")
+print("""You'll be asked a bunch of questions
+then we'll make you up an amazing
+song, totally copyright free 😭""")
+print()
+person = input("Name a person famous for something good: ")
+thing = input("Name a thing they did: ")
+place = input("Name a place you like: ")
+rhyme = input("Give me a verb that rhymes with your person's name: ")
+print()
+print("There was a person called" name)
+print("Who did something cool like", thing, "at the wonderful", place "where you'll find me", rhyme)
+
+//////////// My Code ///////////////
+
+food = input("What meal are we making for dinner?:  ")
+plant = input("Which herb will you add?:  ")
+methodC = input("What method will you cook it with?:  ")
+burn = input("What word describes burned food?:  ")
+item = input("Say home item:  ")
+
+print()
+print("MENU")
+print(methodC, food, "with", burn, plant, "on a bed of", item)
+
+///////////////// Day 3 Finish /////////////////
