fbf65c275c178fa50b092a97feeb20a80f57310e
Beyza Nur ALTINOK <51121202+QueenOfVenus@users.noreply.github.com>
2024-10-21 14:58:30 +0300

    Create 9_Day_ Challenge.py

---
+++ 9_Day_ Challenge.py
@@ -0,0 +1,79 @@
+#////////////////////////////// Comparison Statements Of Python ///////////////////////////////////
+
+# equal
+5 == 5
+# not equal
+3 != 5
+# greater than
+5 > 3
+# greater than or equal to
+5 >= 3
+# less than
+3 < 5
+# less than or equal to
+3 <= 5
+
+myScore = input("Your score: ")
+if myScore > 100000:
+  print("Winner!")
+else:
+  print("Try again 😭")
+
+
+myScore = int(input("Your score: "))
+if myScore > 100000:
+  print("Winner!")
+else:
+  print("Try again 😭")
+
+
+myPi = float(input("What is Pi to 3dp? "))
+if myPi == 3.142 :
+  print("Exactly!")
+else:
+  print("Try again 😭")
+
+
+#//////Fix The Code//////
+
+myPi = float(input("What is Pi to 3dp? ")
+if myPi == 3.142 :
+  print("Exactly!")
+else:
+  print("Try again 😭")
+
+
+myPi = float input("What is Pi to 3dp?")
+if myPi == 3.142 :
+  print("Exactly!)
+else:
+  print("Try again 😭")
+
+score = input("What was your score on the test?"))
+if score >= 80
+  print("Not too shabby")
+elif score > "70":
+  print("Acceptable.")
+else:
+print("Dude, you need to study more!")
+
+#//////////// My Code ///////////////
+
+burn = int(input("Which year were you burn?   :  "))
+if burn >= 1901 and burn <= 1927 :
+  print("Wo wo wow ooo Traditionalists really? My Big Big Grandmaaaa")
+elif burn >= 1928 and burn <= 1945 :
+  print("Ow! Dude you are Baby Boomers.")
+elif burn >= 1946 and burn <= 1964 :
+  print("Who wants to watch Dallas with popcorn?")
+elif burn >= 1965 and burn <= 1980 :
+  print("Who wants to watch Dallas with popcorn?")
+elif burn >= 1981 and burn <= 1996 :
+  print("Hey hey hey do you know super Mario")
+elif burn >= 1997:
+  print("Are you Tiktoker?")
+else :
+  print("I dont understand Dude?")
+
+
+#///////////////// Day 9 Finish /////////////////
