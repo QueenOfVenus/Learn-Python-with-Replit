f9ba19e2268621cd9a05607cb1795d72f3a5406c
Beyza Nur ALTINOK <51121202+QueenOfVenus@users.noreply.github.com>
2024-10-21 13:47:41 +0300

    Create 1_Day_Challenge.py

---
+++ 1_Day_Challenge.py
@@ -0,0 +1,43 @@
+////////////////////////////// Say Hello Of Python ///////////////////////////////////
+print("Well we")
+print("just use more lines")
+print("of code")
+
+print("""Anything that starts
+with three quotes, and ends
+in three quotes can span
+many lines and even contain " symbols
+within it without freaking anything out!""")
+
+//////Fix The Code//////
+
+Print("What could go wrong?")
+
+print "Hello Again"
+
+print(Please work)
+
+print(= MUSIC+ =)
+print("> Songs"
+Print("> Albums")
+print(> Artists")
+
+
+//////////// My Code ///////////////
+
+print("Mark")
+print("October 2nd 2024")
+print("""
+I am sinning up for Replit's 100 days of Python
+challenge!
+I will make sure to spend some time every day coding
+along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do
+this from my phone wherever I happen to be. mo 
+excuses for not coding from the middle of a field!
+""")
+print("I am feeling 😎")
+
+
+///////////////// Day 1 Finish /////////////////
+
