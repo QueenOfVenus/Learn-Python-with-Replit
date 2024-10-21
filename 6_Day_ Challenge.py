3f3afa91811b0d8783f19a383fd4a0576179cfb7
Beyza Nur ALTINOK <51121202+QueenOfVenus@users.noreply.github.com>
2024-10-21 14:45:09 +0300

    Create 6_Day_ Challenge.py

---
+++ 6_Day_ Challenge.py
@@ -0,0 +1,72 @@
+////////////////////////////// If - Elif - Else Statements Of Python ///////////////////////////////////
+
+print("SECURE LOGIN")
+username = input("Username > ")
+if username == "mark":
+  print("Welcome Mark!")
+else:
+  print("Go away!")
+
+
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password> ")
+
+
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password> ")
+
+if username == "mark" and password == "password":
+ print("Welcome Mark!")
+elif username == "suzanne":
+ print("Hey there Suzanne!")
+else:
+ print("Go away!")
+
+//////Fix The Code//////
+
+print("SECURE LOGIN")
+username = input("Username > ")
+
+if username == "mark":
+  print("Welcome Mark!")
+else:
+  print("Go away!")
+elif username == "suzanne":
+  print("Hey there Suzanne!")
+
+
+season = input(what is your favorite season?)
+if season = "spring"
+  print("Ah! The birds are chirping and flowers blooming.")
+  elif season == summer:
+  print("Catch some sun and cool off with a lemonade.")
+elif season == autumn
+print("The leaves are changing and the air is crisp. Enjoy!)
+      elif season = winter:
+      print("Stay warm by the fire and watch the snow fall.")
+else: 
+print("I don't know that season. Please try again.")
+
+//////////// My Code ///////////////
+
+print("My Login System")
+print("***********************")
+print()
+usrnm = input("Username >  ")
+passw = input("Password >  ")
+
+if usrnm == "Dora" and passw == "youKnow":
+  print("Hi Dora! Good for you, you didn't forget the password. ")
+  print("If you had forgotten, I would have had to kill you like the others.")
+  print(" Luckily you knew haha... XD")
+
+elif usrnm == "Dora" and passw == "yuoKnow":
+  print("Nice try Script Kiddie")
+
+else:
+  print("╭∩╮( •̀_•́ )╭∩╮ = Fuck Off , BANG! BANG!")
+
+
+///////////////// Day 6 Finish /////////////////
