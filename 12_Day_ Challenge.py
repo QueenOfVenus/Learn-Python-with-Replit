ee149021329aad65d678c6ef4665fbe3baedcf68
Beyza Nur ALTINOK <51121202+QueenOfVenus@users.noreply.github.com>
2024-11-03 17:31:19 +0300

    Create 12_Day_ Challenge.py

---
+++ 12_Day_ Challenge.py
@@ -0,0 +1,37 @@
+# ////////////////////////////// Some Error Problems On Python ///////////////////////////////////
+
+# //////Fix The Code//////
+
+print("100 Days of Code QUIZ")
+print()
+print("How many can you answer correctly?)
+ans1 = ("What language are we writing in?")
+if ans1 == "python":
+  print("Correct")
+else:
+  print("Nope🙈
+ans2 = input("Which lesson number is this?")
+if(ans2>12):
+print("We're not quite that far yet")
+else:
+  print("We've gone well past that!")
+elif(ans2==12):
+  print("That's right!")
+
+
+# __________________________________
+
+# Error 1 :/   SyntaxError: unterminated string literal (detected at line 3)
+
+# Error 2 :/   SyntaxError: unterminated string literal (detected at line 8)
+
+# Error 3 :/   IndentationError: expected an indented block after 'if' statement on line 10
+
+# Error 4 :/   SyntaxError: invalid syntax
+
+# Error 5 :/   IndentationError: expected an indented block after 'else' statement on line 14
+
+# Error 6 :/   TypeError: '>' not supported between instances of 'str' and 'int'
+
+
+# ///////////////// Day x Finish /////////////////
